900393c8255e91661e63f81427494b9b053c6da7
tien.hn <67305927+nanhlua96@users.noreply.github.com>
2021-06-02 17:58:33 +0700

    fix: toggle element

--- packages/react/pages/index.tsx
+++ packages/react/pages/index.tsx
@@ -1,11 +1,3 @@
-import { HelloWorld } from '../src';
-import Toggle from '../src/elements/inputs/toggle';
-
 export default function Home() {
-    return (
-        <div>
-            <HelloWorld />
-            <Toggle />
-        </div>
-    )
+    return (<div />)
 }
