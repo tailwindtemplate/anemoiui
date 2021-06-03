e23ab930d18042dc795bb73ed19662b2ecfb64a6
tien.hn <67305927+nanhlua96@users.noreply.github.com>
2021-06-03 09:39:53 +0700

    feat: just in time mode

--- packages/react/pages/index.tsx
+++ packages/react/pages/index.tsx
@@ -1,3 +1,11 @@
+import Toggle from '../src/elements/inputs/toggle';
+import { useState } from 'react';
+
 export default function Home() {
-    return (<div />)
+    const [toggle, setToggle] = useState(true)
+    return (
+        <div>
+            <Toggle toggle={toggle} setToggle={setToggle} className="bg-secondary-400 !bg-primary-400" />
+        </div>
+    )
 }
--- packages/react/src/elements/inputs/toggle.tsx
+++ packages/react/src/elements/inputs/toggle.tsx
@@ -1,21 +1,24 @@
-import { useState } from 'react';
 import clsx from 'clsx';
 import { Switch } from '@headlessui/react';
 
 type Toggle = {
-    toggle: boolean;
-    setToggle: (toggle: boolean) => void;
+    toggle: boolean
+    setToggle: (toggle: boolean) => void
+    disabled?: boolean
+    className?: string
 }
 
-export default function Toggle({ toggle, setToggle }: Toggle) {
+export default function Toggle({ toggle, setToggle, disabled, className }: Toggle) {
     return (
         <Switch
             className={clsx(
                 toggle ? 'bg-primary-400' : 'bg-gray-200',
-                'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-400'
+                'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-400',
+                className
             )}
             checked={toggle}
             onChange={setToggle}
+            disabled={disabled}
         >
             <span className="sr-only">Use setting</span>
             <span
