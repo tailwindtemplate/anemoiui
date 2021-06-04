d6c5906bcf98cd5e1fb51fd839c2259088b722b7
tien.hn <67305927+nanhlua96@users.noreply.github.com>
2021-06-04 11:39:55 +0700

    fix: modal content to children

--- packages/react/src/components/modal/index.tsx
+++ packages/react/src/components/modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { JSXElementConstructor } from 'react';
+import React, { JSXElementConstructor, PropsWithChildren } from 'react';
 import { Footer } from './footer';
 import { Header } from './header';
 import { Wrapper } from './wrapper';
@@ -8,17 +8,16 @@ type Modal = {
     setOpen: (open: boolean) => void
     theme?: 'dark' | 'light' | string
     title: string
-    content: string
     footerType?: 'left' | 'right' | 'center' | 'between' | string
     confirm?: { onClick: () => void, label: string, IconLeft?: JSXElementConstructor<any>, IconRight?: JSXElementConstructor<any>, className?: string }
     cancel?: { onClick: () => void, label: string, IconLeft?: JSXElementConstructor<any>, IconRight?: JSXElementConstructor<any>, className?: string }
 }
 
-export const Modal = ({ open, setOpen, theme = 'light', title, content, confirm, cancel, footerType }: Modal) => {
+export const Modal = ({ open, setOpen, theme = 'light', title, children, confirm, cancel, footerType }: PropsWithChildren<Modal>) => {
     return (
         <Wrapper open={open} setOpen={setOpen} theme={theme}>
             <Header label={title} theme={theme} />
-            {content}
+            {children}
             <Footer confirm={confirm} cancel={cancel} theme={theme} type={footerType} />
         </Wrapper>
     )
