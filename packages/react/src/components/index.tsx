e5211c53182737fe3641ed54354df96095ffd0c4
phuockaito <57070629+phuockaito@users.noreply.github.com>
2021-06-11 17:34:04 +0700

    feat: user profile

--- packages/react/src/components/index.tsx
+++ packages/react/src/components/index.tsx
@@ -1,2 +1,5 @@
-export * from './modal'
-export * from './tab'
+export * from './modal';
+export * from './tab';
+export * from './carousel';
+export * from './popover';
+export * from './user-profile';
