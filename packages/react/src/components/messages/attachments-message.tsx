3f3b6b4c75d91f1961a16d73b0d3f507706c061b
tien.hn <67305927+nanhlua96@users.noreply.github.com>
2021-06-12 11:25:00 +0700

    fix: split attachment card

--- packages/react/src/components/messages/attachments-message.tsx
+++ packages/react/src/components/messages/attachments-message.tsx
@@ -1,46 +1,6 @@
-import React, { JSXElementConstructor, useMemo } from 'react';
-import { DocumentIcon, DownloadIcon, FastForwardIcon } from '@heroicons/react/outline';
+import React from 'react';
 import clsx from 'clsx';
-
-type AttachmentCard = {
-    type: string
-    fileName: string
-    content: string
-    size?: string
-    url: string
-    Icon?: JSXElementConstructor<any>
-    image?: string
-    hasDownload?: boolean
-    forwardClick?: (e: any) => void
-    onClick?: (e: any) => void
-    beforeHeader?: string
-}
-const AttachmentCard = ({ hasDownload = true, fileName, content, size, url, Icon, image, forwardClick, onClick, beforeHeader }: AttachmentCard) => {
-    const delegate = useMemo(() => {
-        if (image) return <img src={image} className="w-auto max-h-40 md:w-10 md:h-10 rounded-md flex-shrink-0"/>
-        if (Icon) return <Icon className="w-10 h-10 flex-shrink-0" />
-        return <DocumentIcon className="w-10 h-10 flex-shrink-0" />
-    }, [Icon, image])
-    return (
-        <div className={clsx('rounded-3xl border border-gray-200 hover:border-primary-400 px-5 py-3 flex flex-col md:flex-row md:justify-between items-center bg-white', onClick && 'cursor-pointer')} onClick={onClick}>
-            {delegate}
-            <div className="md:mr-auto md:ml-3 w-full overflow-hidden">
-                {beforeHeader && <div className="text-xs text-gray-400 whitespace-nowrap overflow-ellipsis overflow-hidden">{beforeHeader}</div>}
-                <h3 className={clsx('whitespace-nowrap overflow-ellipsis overflow-hidden', beforeHeader ? 'text-primary-300 text-xs' : 'text-gray-600 text-lg')}>{fileName}</h3>
-                <div className={clsx('flex flex-wrap md:flex-nowrap overflow-hidden overflow-ellipsis', beforeHeader ? 'text-xs' : 'text-sm')}>
-                    <div className="text-gray-600 whitespace-nowrap overflow-hidden overflow-ellipsis">{content}</div>
-                    {size && <div className="flex-1 text-gray-400 md:ml-2 whitespace-nowrap">{size}</div>}
-                </div>
-            </div>
-            {hasDownload && (
-                <div className="text-gray-400 mt-auto flex space-x-2 ml-2">
-                    <DownloadIcon className="hover:text-primary-400 w-4 h-4 cursor-pointer" onClick={() => window.open(url, '_blank')} />
-                    <FastForwardIcon className="hover:text-primary-400 w-4 h-4 cursor-pointer" onClick={forwardClick} />
-                </div>
-            )}
-        </div>
-    )
-}
+import { AttachmentCard } from '../cards/attachment-card';
 
 type AttachmentsMessage = {
     attachments: AttachmentCard[]
