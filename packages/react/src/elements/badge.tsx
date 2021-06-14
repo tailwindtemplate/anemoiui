02cb3458a915ab5972e61dcecd3647fa80637d3f
yennhi <yennhi97263@gmail.com>
2021-06-14 11:35:18 +0700

    - feat: avatar's badge

--- packages/react/src/elements/badge.tsx
+++ packages/react/src/elements/badge.tsx
@@ -8,17 +8,25 @@ type Badge = {
     position?: string;
 };
 
-export function Badge({ count = 1, size = 'MD', className, position }: Badge) {
+const POSITION: any = {
+    'top-left': '-top-1 -left-1',
+    'top-right': '-top-1 -right-1',
+    'bottom-left': '-bottom-1 -left-1',
+    'bottom-right': '-bottom-1 -right-1',
+};
+
+export function Badge({ count = 1, className, position = 'top-right' }: Badge) {
     return (
         <span
             className={clsx(
-                'flex justify-center items-center rounded-full leading-4 whitespace-no-wrap text-tiny w-4 h-4',
+                'flex justify-center items-center rounded-full leading-4 whitespace-no-wrap text-tiny w-4 h-4 border border-white',
                 className,
                 position && 'absolute',
-                count > 9 && 'px-3'
+                POSITION[position],
+                count > 9 && 'px-4'
             )}
         >
-            {count > 99 ? '99+' : count}
+            {count > 999 ? '999+' : count}
         </span>
     );
 }
