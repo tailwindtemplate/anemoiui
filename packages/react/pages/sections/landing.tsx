e785567e960f713d773e2040d9ad362a9d849249
yennhi <yennhi97263@gmail.com>
2021-06-15 09:02:05 +0700

    - fix: download section

--- packages/react/pages/sections/landing.tsx
+++ packages/react/pages/sections/landing.tsx
@@ -1,11 +1,9 @@
-import {Avatar, Badge, Button, Section} from "../../src";
+import { Button } from "../../src";
 
 export default function LandingSection() {
     return (
         <div className="relative bg-white overflow-hidden">
-            <div className="max-w-7xl mx-auto">
                 <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-
                     <div>
                         <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                             <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start"
@@ -24,14 +22,11 @@ export default function LandingSection() {
                             </p>
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                     <Button title='Google Store' type='secondary' />
-                                <div className="mt-3 sm:mt-0 sm:ml-3">
-                                    <Button title={'App store'} type='secondary' />
-                                </div>
+                                    <Button title='App store' type='secondary' className='mt-3 sm:mt-0 sm:ml-3' />
                             </div>
                         </div>
                     </main>
                 </div>
-            </div>
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                 <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
                      src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
