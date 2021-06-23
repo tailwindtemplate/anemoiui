437323965b9e732be7044a68e1bba93c6a8dfc0a
tien.hn <67305927+nanhlua96@users.noreply.github.com>
2021-06-23 15:20:29 +0700

    fix: eslint

--- packages/react/pages/components/notification.tsx
+++ packages/react/pages/components/notification.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import clsx from 'clsx'
 
 import { carouselUser } from '../../const'
@@ -7,11 +6,12 @@ import { PopoverControl } from '../../src/'
 import { ChevronDownIcon } from '@heroicons/react/outline'
 import { ChatAlt2Icon, CogIcon } from '@heroicons/react/outline'
 import { FooterNotification, HeaderNotification } from '../../src/components/notification-modal'
+import { useCallback, useState } from 'react';
 
-export default function NotifiCation() {
-    const [userId, setUserId] = React.useState<string>(null)
-    const active = React.useCallback((id) => setUserId(id), [userId])
-    const [tick, setTick] = React.useState<boolean>(false)
+export default function Notification() {
+    const [userId, setUserId] = useState<string>(null)
+    const active = useCallback((id) => setUserId(id), [userId])
+    const [tick, setTick] = useState<boolean>(false)
     const iconClassName ='text-gray-500 w-5 h-5 mr-2 cursor-pointer transform hover:text-gray-700'
     const footerClassName ='px-4 py-2 text-blue-500 text-sm text-sm cursor-pointer'
     return (
@@ -20,40 +20,40 @@ export default function NotifiCation() {
             Icon={ChevronDownIcon}
             classPopoverPanel='flex flex-col border z-10 transform left-0pt-2 rounded-lg shadow-xl mt-2 w-96 h-96 overflow-hidden'
         >
-                <HeaderNotification title='Messenger' className='border-b' titleClassName='text-lg font-semibold p-2'>
-                    <CogIcon className={iconClassName} />
-                    <ChatAlt2Icon className={iconClassName} />
-                </HeaderNotification>
-                <div className='bg-white overflow-y-auto flex-1'>
+            <HeaderNotification title='Messenger' className='border-b' titleClassName='text-lg font-semibold p-2'>
+                <CogIcon className={iconClassName} />
+                <ChatAlt2Icon className={iconClassName} />
+            </HeaderNotification>
+            <div className='bg-white overflow-y-auto flex-1'>
                 {carouselUser.map(({ id, img }) => (
-                        <UserCard
-                            content='utilities for controlling the radius of an element'
-                            time='09:07'
-                            className={clsx(
-                                'rounded m-2 rounded-sm transform hover:bg-gray-100 cursor-pointer flex-1',
-                                id === userId && 'bg-gray-200',
-                                tick && 'bg-gray-200'
-                            )}
-                            avatar={{ avatar: img }}
-                            title={id}
-                            key={id}
-                            onClick={() => active(id)}
-                            avatarClassName='rounded-full'
-                        />
-                    ))}
-                </div>
-                <FooterNotification className='border-t'>
-                    <p className={footerClassName}>Mở bằng messenger</p>
-                    <p
-                        className={footerClassName}
-                        onClick={() => {
-                            setTick(!tick)
-                            setUserId(null)
-                        }}
-                    >
+                    <UserCard
+                        content='utilities for controlling the radius of an element'
+                        time='09:07'
+                        className={clsx(
+                            'rounded m-2 rounded-sm transform hover:bg-gray-100 cursor-pointer flex-1',
+                            id === userId && 'bg-gray-200',
+                            tick && 'bg-gray-200'
+                        )}
+                        avatar={{ avatar: img }}
+                        title={id}
+                        key={id}
+                        onClick={() => active(id)}
+                        avatarClassName='rounded-full'
+                    />
+                ))}
+            </div>
+            <FooterNotification className='border-t'>
+                <p className={footerClassName}>Mở bằng messenger</p>
+                <p
+                    className={footerClassName}
+                    onClick={() => {
+                        setTick(!tick)
+                        setUserId(null)
+                    }}
+                >
                     Đánh dấu tất cả chưa đọc
-                    </p>
-                </FooterNotification>
+                </p>
+            </FooterNotification>
         </PopoverControl>
     )
 }
