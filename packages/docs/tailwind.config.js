66e30fe5bbfcdff32944575e0254b7f9cd6793fd
Hiep Thai <thaichanhiep@gmail.com>
2021-09-24 00:17:35 +0700

    fix(docs) add tailwindcss purge config

--- packages/docs/tailwind.config.js
+++ packages/docs/tailwind.config.js
@@ -1,5 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 module.exports = {
-    purge: [],
+    purge: {
+        content: [
+            // external paths
+            `${path.dirname(require.resolve('@anemoiui/react/package.json'))}/**/*.js`,
+
+            // internal paths
+            './src/**/*.tsx',
+        ],
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
