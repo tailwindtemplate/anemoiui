5c060c41c8f6fa0e905c4953e8f5e550d79c3bb1
Hiep Thai <thaichanhiep@gmail.com>
2021-09-24 09:39:51 +0700

    chore(docs) update docs preview

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { Card } from '@anemoiui/react/components'
-import { TextInput } from '@anemoiui/react/elements'
+import { Card, List } from '@anemoiui/react/components'
+import { Button } from '@anemoiui/react/elements'
 
 export default function HomePage(): JSX.Element {
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center">
-            <Card>
-                <h2 className="text-2xl uppercase">Anemoi UI Documentation</h2>
-                <span className="text-sm text-gray-500">Welcome to our documentation page.</span>
+        <div className="min-h-screen flex flex-col justify-center items-center space-y-8">
+            <Card className="space-y-4">
+                <div className="block">
+                    <h2 className="text-2xl uppercase">Anemoi UI Documentation</h2>
+                    <span className="text-sm text-gray-500">Welcome to our documentation page.</span>
+                </div>
             </Card>
-            <TextInput />
+            <List.Unordered className="flex flex-row space-x-4">
+                <List.Item>
+                    <Button.Link href="#1">Documentation</Button.Link>
+                </List.Item>
+                <List.Item>
+                    <Button.Link href="#2">Get Started</Button.Link>
+                </List.Item>
+            </List.Unordered>
         </div>
     )
 }
